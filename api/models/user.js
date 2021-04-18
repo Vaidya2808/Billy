f9262014f8196781ab411b4df617e72dc0f3ce2e
Abhimanyu Vaidya <vaidya.abhinanyu@gmail.com>
2021-04-18 23:04:17 +0530

    Thrid commit

--- api/models/user.js
+++ api/models/user.js
@@ -16,17 +16,17 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  location: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ["Point"], // 'location.type' must be 'Point'
-      required: true,
-    },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
+  // location: {
+  //   type: {
+  //     type: String, // Don't do `{ location: { type: String } }`
+  //     enum: ["Point"], // 'location.type' must be 'Point'
+  //     required: true,
+  //   },
+  //   coordinates: {
+  //     type: [Number],
+  //     required: true,
+  //   },
+  // },
 });
 
 module.exports = mongoose.model('User', userSchema);
